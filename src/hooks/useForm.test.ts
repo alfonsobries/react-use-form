fa474cabde2298ad43b273fd20909aab00cb9816
alfonsobries <alfonsobries@gmail.com>
2022-04-01 19:02:26 -0600

    Testing different alternatives

--- src/hooks/useForm.test.ts
+++ src/hooks/useForm.test.ts
@@ -4,24 +4,31 @@ import { describe, expect, it } from 'vitest';
 import { useForm } from './useForm';
 
 describe('useForm', () => {
-  it('should add the form properties', async () => {
-    const data = {
-      name: 'Alfonso',
-      email: 'alfonso@vexilo.com',
-      password: 'secret',
-      remember: true,
-    };
+  const data = {
+    name: 'Alfonso',
+    email: 'alfonso@vexilo.com',
+    password: 'secret',
+    remember: true,
+  };
 
+  it('should add the form properties', () => {
     const {
       result: { current: form },
-    } = await renderHook(() => useForm(data));
-
-    // act(() => {
-    //   result.current.increment()
-    // })
+    } = renderHook(() => useForm(data));
 
     Object.entries(data).forEach(([key, value]) => {
       expect(form[key]).toBe(value);
     });
+  });
+
+  it('updates the value of a property', () => {
+    const { result } = renderHook(() => useForm(data));
+
+    act(() => {
+      result.current.set('name', 'Saida');
+    });
+
+    expect(result.current.name).toBe('Saida');
+    expect(result.current.email).toBe('alfonso@vexilo.com');
   });
 });
--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,9 +1,50 @@
 // import { useCallback, useState } from 'react';
 
-import Form from '../form/Form';
+import { useCallback, useReducer } from 'react';
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case 'set':
+      return { ...state, ...payload };
+    default:
+      throw new Error();
+  }
+}
 
 export const useForm = (data: Record<string, unknown>) => {
-  const form = new Form(data);
+  const [state, dispatch] = useReducer(reducer, data);
 
-  return form;
+  const [errorsState, errorsDispatch] = useReducer(reducer, {});
+
+  const set = useCallback(
+    (prop: string, value: any) => {
+      dispatch({
+        type: 'set',
+        payload: {
+          [prop]: value,
+        },
+      });
+
+      return value;
+    },
+    [dispatch],
+  );
+
+  const errors = new Proxy(errorsState, {
+    get(target, prop) {},
+  });
+
+  return new Proxy(state, {
+    get(state, attribute: string) {
+      if (attribute === 'set') {
+        return set;
+      }
+
+      if (attribute === 'errors') {
+        return errors;
+      }
+
+      return state[attribute];
+    },
+  });
 };
