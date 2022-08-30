3b97768f3629549ec0099b5faad57326c60d7c01
alfonsobries <alfonsobries@gmail.com>
2022-08-29 19:04:02 -0500

    Export form and error types

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ import { useForm } from './hooks/useForm';
 
 export { FormContext };
 
+export type { default as Errors } from './form/Errors';
+export type { default as Form, FormState, Progress } from './form/Form';
+
 export default useForm;
