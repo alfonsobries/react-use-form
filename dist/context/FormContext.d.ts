02e2b6e025795b886f151bb5965dd92949018865
Rob Trehy <robert.trehy@outlook.com>
2024-07-15 15:43:40 +0100

    Remove dist

--- dist/context/FormContext.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { AxiosInstance } from 'axios';
-declare const FormContext: import("react").Context<AxiosInstance | undefined>;
-export default FormContext;
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import FormContext from './context/FormContext';
-import { useForm } from './hooks/useForm';
-export { FormContext };
-export type { default as Errors } from './form/Errors';
-export type { FormState, Progress } from './form/Form';
-export type { Form } from './hooks/useForm';
-export default useForm;
